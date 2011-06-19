9d68e552f3bd1f1db45e38c3b0f726423c73b79a
Grant McLean <grant@catalyst.net.nz>
2011-06-19 14:28:38 +1200

    fix syntax error

--- lib/Authen/NZigovt/ServiceProvider/CertFactory.pm
+++ lib/Authen/NZigovt/ServiceProvider/CertFactory.pm
@@ -170,7 +170,7 @@ sub _generate_certificate {
 sub _gen_serial {
     my @h = qw(0 1 2 3 4 5 6 7 8 9 a b c d e f);
 
-    return '0x' . join '', @h[rand(8)], map { @h[rand(16)] } (1..15);
+    return '0x' . join '', $h[rand(8)], map { $h[rand(16)] } (1..15);
 }
 
 
